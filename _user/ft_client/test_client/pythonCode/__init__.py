44ed90dbcf3ac1f011be2e9e0ba8d06a43e34328
eq19 <eq19@users.noreply.github.com>
2025-06-25 01:49:45 +0000

    update workflows

--- _user/ft_client/test_client/pythonCode/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    "jello",      # refers to the 'jello.py' file
-    "compile",    # refers to the 'compile.py' file
-    "utilities",  # refers to the 'utilities.py' file
-    #"reverse",   # !!! refers to the 'reverse' function now !!!
-]
